0fa17802566fa4e2ae5f35c5d30a0581e816bc11
Davronnn <kifleader@gmail.com>
2022-12-27 18:56:16 +0500

    branch2

--- js/problem.js
+++ js/problem.js
@@ -3,13 +3,17 @@ const array = ['1', '2', '3', '4', '1', '3', '5'];
 function countElementFromArray(array) {
 	const result = {};
 
-          
+	for (let i = 0; i < array.length; i++) {
+		const element = array[i];
 
-	return {
-		1: 2,
-		2: 1,
-		3: 2,
-		4: 1,
-		5: 1,
-	};
+		if (!result[element]) {
+			console.log(result);
+			result[element] = 1;
+		} else {
+			result[element] += 1;
+		}
+	}
+
+	return result;
 }
+console.log(countElementFromArray(array));
